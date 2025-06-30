38f3ba07c428cf7522acc0e455281cfdc4d86b29
lavinianitsche <lavinia.nitsche@escola.pr.gov.br>
2025-06-30 11:31:06 -0300

    teste js

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function logar() {
+    const email = document.getElementById("email").value;
+    console.log(email)
+
+    const password = document.getElementById("senha").value;
+    console.log(password)
+     
+}
